c61ab185f6ca11be51604fc1812ef4e47cbdddbf
Lucas Severino <80589359+lucs-severino@users.noreply.github.com>
2025-07-30 00:21:32 -0300

    Melhoras no Usuario.cs

--- PlataformaSaber.Domain/Entities/Usuario.cs
+++ PlataformaSaber.Domain/Entities/Usuario.cs
@@ -1,9 +1,9 @@
 public class Usuario
 {
     public Guid Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string SenhaHash { get; set; } = string.Empty;
+    public string Nome { get; set; } 
+    public string Email { get; set; } 
+    public string SenhaHash { get; set; } 
     public UsuarioTipo Tipo { get; set; }
     public UsuarioStatus Status { get; set; }
 
