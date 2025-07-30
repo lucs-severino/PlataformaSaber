627385aa4ecb64a3af4338952ff7ebcc1839772e
Lucas Severino <80589359+lucs-severino@users.noreply.github.com>
2025-07-30 00:20:31 -0300

    Atualizar o Usuario.cs

--- PlataformaSaber.Domain/Entities/Usuario.cs
+++ PlataformaSaber.Domain/Entities/Usuario.cs
@@ -6,4 +6,16 @@ public class Usuario
     public string SenhaHash { get; set; } = string.Empty;
     public UsuarioTipo Tipo { get; set; }
     public UsuarioStatus Status { get; set; }
+
+    public Usuario(Guid id, string nome, string email, string senhaHash, UsuarioTipo tipo, UsuarioStatus status)
+    {
+        Id = id;
+        Nome = nome;
+        Email = email;
+        SenhaHash = senhaHash;
+        Tipo = tipo;
+        Status = status;
+    }
+    
+    public Usuario () {}
 }
