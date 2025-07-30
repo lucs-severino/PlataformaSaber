581fd66b2961fcd95f4dd75d35100d8ae4e61628
lucs-severino <lucs.severino@outlook.com>
2025-07-29 22:38:01 -0300

    tipos de usuarios

---
+++ PlataformaSaber.Domain/Entities/Usuario.cs
@@ -0,0 +1,9 @@
+public class Usuario
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string SenhaHash { get; set; } = string.Empty;
+    public UsuarioTipo Tipo { get; set; }
+    public UsuarioStatus Status { get; set; }
+}
---
+++ PlataformaSaber.Domain/Enums/UsuarioTipo.cs
@@ -0,0 +1,6 @@
+public enum UsuarioTipo
+{
+    Admin,
+    Aluno,
+    Professor
+}
