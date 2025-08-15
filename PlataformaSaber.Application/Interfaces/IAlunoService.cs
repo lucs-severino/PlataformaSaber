afa7d8abf68b9ae413141fc0913c91b351d56a15
Lucas Severino <80589359+lucs-severino@users.noreply.github.com>
2025-08-15 16:10:17 -0300

    feat: adiciona autenticação de login no serviço e interface AlunoService

--- PlataformaSaber.Application/Interfaces/IAlunoService.cs
+++ PlataformaSaber.Application/Interfaces/IAlunoService.cs
@@ -1,5 +1,15 @@
+// IAlunoService.cs
 
-public interface IAlunoService : IService<AlunoDto>
+using System.Threading.Tasks;
+
+namespace PlataformaSaber.Application.Interfaces
 {
+    public interface IAlunoService
+    {
+        Task<AlunoDto> GetAlunoByIdAsync(int id);
+        Task<IEnumerable<AlunoDto>> GetAllAlunosAsync();
+        Task AddAlunoAsync(AlunoDto aluno);
+        Task UpdateAlunoAsync(AlunoDto aluno);
+        Task DeleteAlunoAsync(int id);
+    }
 }
-
